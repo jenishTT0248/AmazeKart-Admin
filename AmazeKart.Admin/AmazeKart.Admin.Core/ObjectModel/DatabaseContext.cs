e829614e0a0a892fc18e678c4923635d9a28240c
Jenish Pachchigar <131193115+jenishTT0248@users.noreply.github.com>
2023-04-19 23:57:30 +0530

    Dev to main (#13)
    * Added testing controller
    
    * deleted testing controller
    
    * testing controller (#11)
    
    * Prepared database Object model of Category, Product, Payment Type and supplier (#12)
    
    Co-authored-by: Jenish <Jenish@JENISH>
    
    ---------
    
    Co-authored-by: Jenish <Jenish@JENISH>

---
+++ AmazeKart.Admin/AmazeKart.Admin.Core/ObjectModel/DatabaseContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AmazeKart.Admin.Core.ObjectModel
+{
+    public partial class AmazeKartDB : DbContext
+    {
+        public string connectionString;
+
+        public AmazeKartDB(DbContextOptions<AmazeKartDB> dbContextOptions)
+              : base(dbContextOptions)
+        {
+        }
+
+        #region AmazeKart
+        public virtual DbSet<Category> Categories { get; set; }
+        public virtual DbSet<PaymentType> PaymentTypes { get; set; }
+        public virtual DbSet<Product> Products { get; set; }
+        public virtual DbSet<Supplier> Suppliers { get; set; }
+        #endregion
+    }
+}
