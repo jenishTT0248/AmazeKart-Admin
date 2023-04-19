c74bb3d8c684794331b03ee50e6c893268b82d78
Jenish Pachchigar <131193115+jenishTT0248@users.noreply.github.com>
2023-04-19 23:55:02 +0530

    Prepared database Object model of Category, Product, Payment Type and supplier (#12)
    Co-authored-by: Jenish <Jenish@JENISH>

---
+++ AmazeKart.Admin/AmazeKart.Admin.Core/ObjectModel/Category.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.Serialization;
+
+namespace AmazeKart.Admin.Core.ObjectModel
+{
+    [DataContract]
+    public partial class Category
+    {
+        public Category()
+        {
+        }
+
+        [DataMember]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int CategoryId { get; set; }
+        [DataMember]
+        public string CategoryName { get; set; }
+        [DataMember]
+        public string Description { get; set; }
+        [DataMember]
+        public bool Active { get; set; }
+
+    }
+}
---
+++ AmazeKart.Admin/AmazeKart.Admin.Core/ObjectModel/PaymentType.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AmazeKart.Admin.Core.ObjectModel
+{
+    [DataContract]
+    public partial class PaymentType
+    {
+        public PaymentType()
+        {
+        }
+
+        [DataMember]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int PaymentId { get; set; }
+
+        [DataMember]
+        public string Type { get; set; }
+    }
+}
