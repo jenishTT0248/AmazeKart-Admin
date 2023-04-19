71f3d34079b9f2c8aa0b41840fa7ad0c9d80cee0
Jenish Pachchigar <jenish.pachchigar@triveniglobalsoft.com>
2023-04-19 16:17:40 +0530

    Initial commit

---
+++ AmazeKart.Admin/AmazeKart.Admin.WinService/Program.cs
@@ -0,0 +1,10 @@
+using AmazeKart.Admin.WinService;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
