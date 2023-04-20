1c1f5455199707820b8683774d503a9022237fcc
Jenish Pachchigar <131193115+jenishTT0248@users.noreply.github.com>
2023-04-20 23:43:40 +0530

    Create architecture (#14)
    Co-authored-by: Jenish <Jenish@JENISH>

---
+++ AmazeKart.Admin/AmazeKart.Admin.Infrastructure/Repositories/OrderDetailRepository.cs
@@ -0,0 +1,13 @@
+﻿using AmazeKart.Admin.Core.IRepositories;
+using AmazeKart.Admin.Core.ObjectModel;
+
+namespace AmazeKart.Admin.Infrastructure.Repositories
+{
+    public class OrderDetailRepository : Repository<OrderDetail>, IOrderDetailRepository
+    {
+        public OrderDetailRepository(IUnitOfWork context)
+            : base(context)
+        {
+        }
+    }
+}
---
+++ AmazeKart.Admin/AmazeKart.Admin.Infrastructure/Repositories/ProductCatalogRepository.cs
@@ -0,0 +1,13 @@
+﻿using AmazeKart.Admin.Core.IRepositories;
+using AmazeKart.Admin.Core.ObjectModel;
+
+namespace AmazeKart.Admin.Infrastructure.Repositories
+{
+    public class ProductCatalogRepository : Repository<ProductCatalog>, IProductCatalogRepository
+    {
+        public ProductCatalogRepository(IUnitOfWork context)
+            : base(context)
+        {
+        }
+    }
+}
