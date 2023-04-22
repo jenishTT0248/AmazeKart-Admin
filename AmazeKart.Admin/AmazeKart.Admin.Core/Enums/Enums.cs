d95821d9cc9ca9e1bb2f2b44755cf9f98f6f4cd4
Jenish <Jenish@JENISH>
2023-04-22 21:58:18 +0530

    Architecture setup

---
+++ AmazeKart.Admin/AmazeKart.Admin.Core/Enums/Enums.cs
@@ -0,0 +1,40 @@
+﻿using AmazeKart.Admin.Core.Utility;
+
+namespace AmazeKart.Admin.Core.Enums
+{
+    public enum MessageType
+    {
+        [StringValue("Success")]
+        Success = 1,
+        [StringValue("Info")]
+        Information = 2,
+        [StringValue("Warning")]
+        Warning = 3,
+        [StringValue("Error")]
+        Error = 4
+    }
+
+    public enum ResultMessage
+    {
+        [StringValue("Record Not Inserted")]
+        Fail = 0,
+        [StringValue("Success!")]
+        Success = 1,
+        [StringValue("This record already exists.")]
+        RecordExists = 2,
+        [StringValue("Record not found: Please edit your search and try again.")]
+        RecordNotFound = 3,
+        [StringValue("Your username and password were not recognized. Please review your inputs and try again.")]
+        InvalidUserName = 4
+    }
+
+    public enum MessageConstants
+    {
+        [StringValue("Record inserted successfully.")]
+        RecordInsertSuccessfully,
+        [StringValue("Record updated successfully.")]
+        RecordupdateSuccessfully,
+        [StringValue("Record deleted successfully.")]
+        RecordDeleteSuccessfully
+    }
+}
