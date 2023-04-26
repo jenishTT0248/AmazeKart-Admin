0e0fcdda80fba51e20d4a1493932c87e09f1a04e
Jenish <Jenish@JENISH>
2023-04-26 23:29:13 +0530

    Create product screen related API

---
+++ AmazeKart.Admin/AmazeKart.Admin.Core/IBal/IProductBAL.cs
@@ -0,0 +1,13 @@
+﻿using AmazeKart.Admin.Core.Enums;
+
+namespace AmazeKart.Admin.Core.IBal
+{
+    public interface IProductBAL
+    {
+        ResultMessage Create(ViewModel.Product entity);
+        ResultMessage Update(ViewModel.Product entity);
+        ResultMessage Delete(int productId);
+        IQueryable<ViewModel.Product> GetAll();
+        ViewModel.Product GetById(int productId);
+    }
+}
