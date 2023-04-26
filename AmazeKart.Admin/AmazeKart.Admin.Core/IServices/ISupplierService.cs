1f804a459e4601a5c1556e7c8e9170a97efff4bf
Jenish Pachchigar <131193115+jenishTT0248@users.noreply.github.com>
2023-04-26 22:32:42 +0530

    Create supplier screen WebAPI (#37)
    * Supplier API (WIP)
    
    * Supplier API [WIP]
    
    * Code refactor for supplier screen
    
    * BaseAPI controller changes
    
    ---------
    
    Co-authored-by: kushagra.makharia@triveniglobalsoft.com <kushagra.makharia@triveniglobalsoft.com>
    Co-authored-by: Jenish <Jenish@JENISH>

---
+++ AmazeKart.Admin/AmazeKart.Admin.Core/IServices/ISupplierService.cs
@@ -0,0 +1,15 @@
+﻿using AmazeKart.Admin.Core.Enums;
+using AmazeKart.Admin.Core.ObjectModel;
+
+
+namespace AmazeKart.Admin.Core.IServices
+{
+    public interface ISupplierService
+    {
+        ResultMessage Create(Supplier supplier);
+        ResultMessage Update(Supplier supplier);
+        ResultMessage Delete(int supplierId);
+        Supplier GetById(int supplierId);
+        IQueryable<Supplier> GetAll();
+    }
+}
