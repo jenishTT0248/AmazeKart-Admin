63c520639d59d6c4cd7900f8df91c85144c63d9e
Jenish Pachchigar <131193115+jenishTT0248@users.noreply.github.com>
2023-04-26 23:34:00 +0530

    Dev to main (#40)
    * Added testing controller
    
    * deleted testing controller
    
    * testing controller (#11)
    
    * Prepared database Object model of Category, Product, Payment Type and supplier (#12)
    
    Co-authored-by: Jenish <Jenish@JENISH>
    
    * Create architecture (#14)
    
    Co-authored-by: Jenish <Jenish@JENISH>
    
    * Resolved error
    
    * resolve error
    
    * Added Flowcharts (#18)
    
    * Added some logos
    
    * Architecture setup (#21)
    
    Co-authored-by: Jenish <Jenish@JENISH>
    
    * Architectute setup (#23)
    
    Co-authored-by: Jenish <Jenish@JENISH>
    
    * Object model changes
    
    * code refactor
    
    * Postman collection updated
    
    * logo added
    
    * Changes in Order, order detail, payment type, customer and category Web APIs (#31)
    
    Co-authored-by: Jenish <Jenish@JENISH>
    
    * Create supplier screen WebAPI (#37)
    
    * Supplier API (WIP)
    
    * Supplier API [WIP]
    
    * Code refactor for supplier screen
    
    * BaseAPI controller changes
    
    ---------
    
    Co-authored-by: kushagra.makharia@triveniglobalsoft.com <kushagra.makharia@triveniglobalsoft.com>
    Co-authored-by: Jenish <Jenish@JENISH>
    
    * Create product screen related WebAPI (#39)
    
    * Create product screen related API
    
    * supplier screen related changes
    
    ---------
    
    Co-authored-by: Jenish <Jenish@JENISH>
    
    ---------
    
    Co-authored-by: Jenish <Jenish@JENISH>
    Co-authored-by: TGSkushagra <95115405+TGSkushagra@users.noreply.github.com>
    Co-authored-by: kushagra.makharia@triveniglobalsoft.com <kushagra.makharia@triveniglobalsoft.com>

---
+++ AmazeKart.Admin/AmazeKart.Admin.Core/ViewModel/Product.cs
@@ -0,0 +1,24 @@
+﻿namespace AmazeKart.Admin.Core.ViewModel
+{
+    public class Product
+    {
+        public Product()
+        {
+        }
+
+        public int Id { get; set; }
+        public string SKU { get; set; }
+        public string ProductName { get; set; }
+        public int SupplierId { get; set; }
+        public int CategoryId { get; set; }
+        public string Size { get; set; }
+        public string Color { get; set; }
+        public string UnitWeight { get; set; }
+        public string UnitsInStock { get; set; }
+        public string DiscountType { get; set; }
+        public decimal DiscountValue { get; set; }
+        public bool Active { get; set; }
+        public Supplier Supplier { get; set; }
+        public Category Category { get; set; }
+    }
+}
---
+++ AmazeKart.Admin/AmazeKart.Admin.Infrastructure/Bal/ProductBAL.cs
@@ -0,0 +1,59 @@
+﻿using AmazeKart.Admin.Core.Enums;
+using AmazeKart.Admin.Core.IBal;
+using AmazeKart.Admin.Core.IServices;
+using AutoMapper;
+using ObjectModel = AmazeKart.Admin.Core.ObjectModel;
+using ViewModel = AmazeKart.Admin.Core.ViewModel;
+
+namespace AmazeKart.Admin.Infrastructure.Bal
+{
+    public class ProductBAL : IProductBAL
+    {
+        private readonly IProductService _productService;
+        private readonly IMapper _mapper;
+
+        public ProductBAL(IMapper mapper, IProductService productService)
+        {
+            _productService = productService;
+            _mapper = mapper;
+        }
+
+        public ResultMessage Create(ViewModel.Product entity)
+        {
+            if (entity == null) return ResultMessage.RecordNotFound;
+
+            ObjectModel.Product product = new ObjectModel.Product();
+            _mapper.Map<ViewModel.Product, ObjectModel.Product>(entity, product);
+            return _productService.Create(product);
+        }
+
+        public ResultMessage Update(ViewModel.Product entity)
+        {
+            if (entity == null) return ResultMessage.RecordNotFound;
+
+            ObjectModel.Product product = new ObjectModel.Product();
+            _mapper.Map<ViewModel.Product, ObjectModel.Product>(entity, product);
+            return _productService.Update(product);
+        }
+
+        public ResultMessage Delete(int productId)
+        {
+            return _productService.Delete(productId);
+        }
+
+        public IQueryable<ViewModel.Product> GetAll()
+        {
+            var products = _productService.GetAll().ToList();
+            List<ViewModel.Product> productList = new();
+            productList = _mapper.Map<List<ObjectModel.Product>, List<ViewModel.Product>>(products);
+            return productList.AsQueryable();
+        }
+
+        public ViewModel.Product GetById(int productId)
+        {
+            ObjectModel.Product product = _productService.GetById(productId);
+            ViewModel.Product productViewModel = _mapper.Map<ObjectModel.Product, ViewModel.Product>(product);
+            return productViewModel;
+        }
+    }
+}
