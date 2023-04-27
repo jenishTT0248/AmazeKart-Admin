68647e14a7a69cce9e6fdb728175edd83bc1c702
Jenish <Jenish@JENISH>
2023-04-27 23:07:55 +0530

    Created cart WebAPI

---
+++ AmazeKart.Admin/AmazeKart.Admin.API/Controllers/CartController.cs
@@ -0,0 +1,68 @@
+﻿using AmazeKart.Admin.Core.Enums;
+using AmazeKart.Admin.Core.IBal;
+using AmazeKart.Admin.Core.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace AmazeKart.Admin.API.Controllers
+{
+    [Route("api/Cart")]
+    public class CartController : BaseAPIController
+    {
+        private readonly ICartBAL _cartBAL;
+
+        public CartController(ICartBAL cartBAL)
+        {
+            _cartBAL = cartBAL;
+        }
+
+        [HttpPost, Route("SaveData")]
+        public IActionResult SaveData(Cart entity)
+        {
+            ResultMessage rMsg = ResultMessage.RecordNotFound;
+            MessageConstants resultMessage;
+
+            if (entity.Id == 0)
+            {
+                resultMessage = MessageConstants.RecordInsertSuccessfully;
+                rMsg = _cartBAL.Create(entity);
+            }
+            else
+            {
+                resultMessage = MessageConstants.RecordupdateSuccessfully;
+                rMsg = _cartBAL.Update(entity);
+            }
+
+            if (rMsg != ResultMessage.Success)
+                return Ok(new ResponseResult(HttpStatusCode.BadRequest, rMsg.GetStringValue(), null, MessageType.Warning.GetStringValue()));
+            return Ok(new ResponseResult(HttpStatusCode.OK, resultMessage.GetStringValue(), null, MessageType.Success.GetStringValue()));
+        }
+
+        [HttpPost, Route("DeleteData")]
+        public IActionResult DeleteData(int cartId)
+        {
+            ResultMessage rMsg = ResultMessage.RecordNotFound;
+            MessageConstants resultMessage = MessageConstants.RecordDeleteSuccessfully;
+            rMsg = _cartBAL.Delete(cartId);
+
+            if (rMsg != ResultMessage.Success)
+                return Ok(new ResponseResult(HttpStatusCode.BadRequest, rMsg.GetStringValue(), null, MessageType.Warning.GetStringValue()));
+
+            return Ok(new ResponseResult(HttpStatusCode.OK, resultMessage.GetStringValue(), null, MessageType.Success.GetStringValue()));
+        }
+
+        [HttpGet, Route("GetAll")]
+        public IActionResult GetAll()
+        {
+            List<Cart> cartList = _cartBAL.GetAll().ToList();
+            return Ok(new ResponseResult(HttpStatusCode.OK, string.Empty, cartList, MessageType.Success.GetStringValue()));
+        }
+
+        [HttpGet, Route("GetById")]
+        public IActionResult GetById(int cartId)
+        {
+            Cart cart = _cartBAL.GetById(cartId);
+            return Ok(new ResponseResult(HttpStatusCode.OK, string.Empty, cart, MessageType.Success.GetStringValue()));
+        }
+    }
+}
---
+++ AmazeKart.Admin/AmazeKart.Admin.Infrastructure/Bal/CartBAL.cs
@@ -0,0 +1,58 @@
+﻿using AmazeKart.Admin.Core.Enums;
+using AmazeKart.Admin.Core.IBal;
+using AmazeKart.Admin.Core.IServices;
+using AutoMapper;
+using ObjectModel = AmazeKart.Admin.Core.ObjectModel;
+using ViewModel = AmazeKart.Admin.Core.ViewModel;
+
+namespace AmazeKart.Admin.Infrastructure.Bal
+{
+    public class CartBAL : ICartBAL
+    {
+        private readonly ICartService _cartService;
+        private readonly IMapper _mapper;
+        public CartBAL(IMapper mapper, ICartService cartService)
+        {
+            _mapper = mapper;
+            _cartService = cartService;
+        }
+
+        public ResultMessage Create(ViewModel.Cart entity)
+        {
+            if (entity == null) return ResultMessage.RecordNotFound;
+
+            ObjectModel.Cart cart = new ObjectModel.Cart();
+            _mapper.Map<ViewModel.Cart, ObjectModel.Cart>(entity, cart);
+            return _cartService.Create(cart);
+        }
+
+        public ResultMessage Update(ViewModel.Cart entity)
+        {
+            if (entity == null) return ResultMessage.RecordNotFound;
+
+            ObjectModel.Cart cart = new ObjectModel.Cart();
+            _mapper.Map<ViewModel.Cart, ObjectModel.Cart>(entity, cart);
+            return _cartService.Update(cart);
+        }
+
+        public ResultMessage Delete(int cartId)
+        {
+            return _cartService.Delete(cartId);
+        }
+
+        public IQueryable<ViewModel.Cart> GetAll()
+        {
+            var cartData = _cartService.GetAll().ToList();
+            List<ViewModel.Cart> cartViewModel = new();
+            cartViewModel = _mapper.Map<List<ObjectModel.Cart>, List<ViewModel.Cart>>(cartData);
+            return cartViewModel.AsQueryable();
+        }
+
+        public ViewModel.Cart GetById(int cartId)
+        {
+            ObjectModel.Cart cart = _cartService.GetById(cartId);
+            ViewModel.Cart cartViewModel = _mapper.Map<ObjectModel.Cart, ViewModel.Cart>(cart);
+            return cartViewModel;
+        }
+    }
+}
