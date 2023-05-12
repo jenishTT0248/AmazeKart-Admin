df7fe6d25c86511f44f557e237a3769666516baa
brijesh.sinojiya <brijesh.sinojiya@triveniglobalsoft.com>
2023-05-12 18:44:44 +0530

    Changed request type for delete customer API.

--- AmazeKart.Admin/AmazeKart.Admin.API/Controllers/CustomerController.cs
+++ AmazeKart.Admin/AmazeKart.Admin.API/Controllers/CustomerController.cs
@@ -44,7 +44,7 @@ namespace AmazeKart.Admin.API.Controllers
             return Ok(new ResponseResult(HttpStatusCode.OK, resultMessage.GetStringValue(), null, MessageType.Success.GetStringValue()));
         }
 
-        [HttpPost, Route("DeleteData")]
+        [HttpGet, Route("DeleteData")]
         public IActionResult Delete(int customerId)
         {
             if (customerId <= 0)
